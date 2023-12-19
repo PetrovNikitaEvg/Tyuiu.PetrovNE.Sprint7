46e7e860a9ebb8320591e60cef33003fa74b4e3e
PetrovNikita a.k Shugizawa <silestrange@gmail.com>
2023-12-19 23:56:29 +0500

    7Sprint

---
+++ Tyuiu.PetrovNE.Sprint7.Project0.V10.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.PetrovNE.Sprint7.Project0.V10.Lib
+{
+    public class DataService
+    {
+    }
+}
---
+++ Tyuiu.PetrovNE.Sprint7.Project0.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PetrovNE.Sprint7.Project0.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PetrovNE.Sprint7.Project0.V10.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e09dde56-db0d-45d6-98f6-c41c59f682a6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
